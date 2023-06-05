690e8d15314928ef317630e2e3affbc2a2b0fcfc
slaweksystem <slaweksystem@gmail.com>
2023-06-05 23:31:49 +0200

    Detector

---
+++ Detector.py
@@ -0,0 +1,52 @@
+from anrp import PyImageSearchANPR
+import imutils
+import cv2
+
+def cleanup_text(text):
+    # strip out non-ASCII text so we can draw the text on the image
+    # using OpenCV
+    return "".join([c if ord(c) < 128 else "" for c in text]).strip()
+
+class Detector:
+    def __init__(self, psm = 7, clear_border = -1):
+        self.anrp = PyImageSearchANPR(debug = -1)
+        self.psm = psm
+        self.clear_border = clear_border
+
+    def detect(self, image):
+
+        license_plates = ""
+
+        # Resize image
+        image = imutils.resize(image, width=600)
+
+        # apply automatic license plate recognition
+        (lpText, lpCnt) = self.anrp.find_and_ocr(image, psm=self.psm,
+            clearBorder=self.clear_border > 0)
+
+        # only continue if the license plate was successfully OCR'd
+        if lpText is not None and lpCnt is not None:
+            # fit a rotated bounding box to the license plate contour and
+            # draw the bounding box on the license plate
+            box = cv2.boxPoints(cv2.minAreaRect(lpCnt))
+            box = box.astype("int")
+            cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+
+            # compute a normal (unrotated) bounding box for the license
+            # plate and then draw the OCR'd license plate text on the
+            # image
+            (x, y, w, h) = cv2.boundingRect(lpCnt)
+            cv2.putText(image, cleanup_text(lpText), (x, y - 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+            # show the output ANPR image
+            #print("[INFO] {}".format(lpText))
+            #cv2.imshow("Output ANPR", image)
+            #cv2.waitKey(0)
+            license_plates = lpText
+
+
+        if license_plates == "":
+            license_plates = "No license plate detected"
+        return image, license_plates
+
--- start_gui.py
+++ start_gui.py
@@ -1,19 +1,17 @@
 import numpy as np
 import gradio as gr
+from Detector import Detector
 
-def process_image(input_img):
-    return input_img, "text\ntext2\ntext3"
+detector = Detector()
 
 demo = gr.Interface(
-    process_image, 
+    detector.detect, 
     gr.Image(), 
     [
         "image", 
         gr.Textbox(lines=4, label="License plates", placeholder="License plates")
     ],
     css= ".gradio-container {background-color: grey}"
-
-
     )
 
 demo.launch()
